e628c8af86bdb3ede54fa4a4ffc6c9dbf75672fc
F4NZ41 <mimeremim@gmail.com>
2022-03-16 21:49:31 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import EventBucket from './components/EventBucket.vue'
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+
+</script>
+
+<template>
+  <event-bucket :events="today">Detta händer idag</event-bucket>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      today: [{title: 'bruh', time: '10.10-20.20', desc: 'a little desc'}]
+    }    
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+</style>
---
+++ src/components/EventBucket.vue
@@ -0,0 +1,45 @@
+<template>
+  <section>
+    <h2><slot></slot></h2>
+    <main>
+      <div v-for="(event, index) in events" v-bind:key="index" class="event">
+        <small>{{ event.time }}</small>
+        <h3>{{ event.title }}</h3>
+        <p>{{ event.desc }}</p>
+      </div>
+    </main>
+  </section>
+</template>
+
+<script>
+export default {
+  props: ["events"],
+};
+</script>
+
+<style scoped>
+
+section {
+  width: 100%;
+}
+
+main {
+  max-width: 800px;
+}
+
+.event {
+  padding: 10px;
+  border-radius: 10px;
+  background-color: #458fff;
+}
+
+small,
+h3,
+p {
+  margin: 0;
+}
+
+h2 {
+  text-align: center;
+}
+</style>
